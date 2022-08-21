e7f196d0743ff9bf8346c5c2622ead55d1b8c6ea
thekauer <thekauer@gmail.com>
2022-08-21 17:36:07 +0200

    feat: :fire: reimplement react-katex

--- components/Latex/Latex.tsx
+++ components/Latex/Latex.tsx
@@ -1,9 +1,75 @@
-import { InlineMath } from "react-katex";
+import { useMemo } from "react";
+import KaTeX from "katex";
+
+const trustHandler = ({
+  command,
+  style,
+}: {
+  command: string;
+  style: string;
+}) => {
+  return command === "\\htmlStyle";
+};
+
+const createMathComponent = (Component: any, { displayMode }: any): any => {
+  /**
+   *
+   * @param {MathComponentProps} props
+   * @returns {ReactNode}
+   */
+  const MathComponent = ({ children, errorColor, math, renderError }: any) => {
+    const formula = math ?? children;
+
+    const { html, error } = useMemo(() => {
+      try {
+        const html = KaTeX.renderToString(formula, {
+          displayMode,
+          errorColor,
+          trust: trustHandler,
+          throwOnError: !!renderError,
+        });
+
+        return { html, error: undefined };
+      } catch (error) {
+        if (error instanceof KaTeX.ParseError || error instanceof TypeError) {
+          return { error };
+        }
+
+        throw error;
+      }
+    }, [formula, errorColor, renderError]);
+
+    if (error) {
+      return renderError ? (
+        renderError(error)
+      ) : (
+        <Component html={`${(error as any).message}`} />
+      );
+    }
+
+    return <Component html={html} />;
+  };
+
+  return MathComponent;
+};
+
+const InternalInlineMath = ({ html }: any) => {
+  return (
+    <span
+      data-testid="react-katex"
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  );
+};
+
+const InlineMath = createMathComponent(InternalInlineMath, {
+  displayMode: false,
+});
 
 interface LatexProps {
   children?: string;
 }
 
 export const Latex = ({ children }: LatexProps) => {
-  return <InlineMath>{children}</InlineMath>;
+  return <InlineMath>{children === " " ? "\\;" : children}</InlineMath>;
 };
