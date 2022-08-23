5f68d2e02e529441567a41377fe0c5aa78fedbed
thekauer <thekauer@gmail.com>
2022-08-23 11:24:54 +0200

    feat: :lipstick: add true/false markers

--- components/Branch/Branch.atoms.tsx
+++ components/Branch/Branch.atoms.tsx
@@ -2,6 +2,7 @@ import styled, { css } from "styled-components";
 import { StyleProps } from "../../style/styleProps";
 
 export const Condition = styled.div<StyleProps>`
+  position: relative;
   padding: 0.5em;
   outline: solid 2px var(--mid);
   outline-offset: -1px;
@@ -32,9 +33,49 @@ export const Condition = styled.div<StyleProps>`
       background-color: var(--blue);
     `}
 `;
+
+export const True = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 1em;
+  text-align: center;
+
+  &::after {
+    position: absolute;
+    left: 0.75em;
+    bottom: -0.3em;
+    content: " ";
+    width: 1.5px;
+    height: 2.2em;
+    background-color: var(--mid);
+    transform: rotate(-45deg);
+  }
+`;
+
+export const False = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 1em;
+  text-align: center;
+
+  &::before {
+    position: absolute;
+    left: 0.125em;
+    bottom: -0.3em;
+    content: " ";
+    width: 1.5px;
+    height: 2.2em;
+    background-color: var(--mid);
+    transform: rotate(45deg);
+  }
+`;
+
 export const Container = styled.div`
   display: flex;
 `;
+
 export const TrueBranch = styled.div`
   outline: solid 2px var(--mid);
   outline-offset: -1px;
