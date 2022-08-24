8df6f724a9ed30efdf42e6c30feeb284d2ad1611
thekauer <thekauer@gmail.com>
2022-08-24 23:04:28 +0200

    feat: :tada: add export page

--- components/SideMenu/Export/Export.tsx
+++ components/SideMenu/Export/Export.tsx
@@ -1,22 +1,49 @@
+import { ChangeEventHandler } from "react";
 import { useScreenshot } from "../../../hooks/useScreenshot";
+import { useTheme } from "../../../hooks/useTheme";
 import * as SM from "../SideMenu.atoms";
 
 export const Export = () => {
-  const [image, takeScreenShot] = useScreenshot();
+  const { downloadScreenshot, screenshotToClipboard } = useScreenshot();
+  const { astTheme, setAstTheme, setShowScope } = useTheme();
+
+  const prepareScreenshot = async (
+    cb: (root: HTMLElement) => Promise<void>
+  ) => {
+    const root = document.getElementById("root-container")
+      ?.firstChild as HTMLElement;
+    if (!root) return;
+
+    setShowScope(false);
+    await cb(root);
+    setShowScope(true);
+  };
 
   const exportClick = () => {
-    const rootContainer = document.getElementById("root-container");
-    if (!rootContainer?.firstChild) {
-      return;
-    }
+    prepareScreenshot(downloadScreenshot);
+  };
 
-    takeScreenShot(rootContainer.firstChild as HTMLElement);
+  const copyToClipboardClick = () => {
+    prepareScreenshot(screenshotToClipboard);
+  };
+
+  const onThemeChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    const theme = e.target.value;
+    setAstTheme(theme);
   };
 
   return (
     <SM.Container>
       <SM.Title>Export</SM.Title>
-      <SM.Button onClick={exportClick}>Export</SM.Button>
+      <SM.Label htmlFor="themes">Theme:</SM.Label>
+      <SM.Select id="themes" onChange={onThemeChange} value={astTheme}>
+        <SM.Option value="dark">Dark</SM.Option>
+        <SM.Option value="light">Light</SM.Option>
+        <SM.Option value="black-on-white">Black on white</SM.Option>
+        <SM.Option value="white-on-black">White on black</SM.Option>
+      </SM.Select>
+      <SM.Button onClick={exportClick}>Download</SM.Button>
+      <SM.Button onClick={copyToClipboardClick}>Copy to Clipboard</SM.Button>
     </SM.Container>
   );
 };
