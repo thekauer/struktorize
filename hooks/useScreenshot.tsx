ada26e1072ca10cc1526600b742e9469c3d23c5a
thekauer <thekauer@gmail.com>
2022-08-24 22:34:43 +0200

    feat: :zap: make useScreenshot functions awaitable

--- hooks/useScreenshot.tsx
+++ hooks/useScreenshot.tsx
@@ -1,19 +1,36 @@
 import { useState } from "react";
-import { toPng } from "html-to-image";
+import { toPng, toBlob } from "html-to-image";
 
 const useScreenshot = () => {
   const [image, setImage] = useState<string>();
   const [error, setError] = useState(null);
 
-  const takeScreenShot = (node: HTMLElement) => {
+  const takeScreenShot = async (node: HTMLElement) => {
     if (!node) {
       throw new Error("You should provide correct html node.");
     }
 
-    return toPng(node).then(setImage).catch(setError);
+    try {
+      const image = await toPng(node);
+      setImage(image);
+    } catch (e) {
+      setError(e as any);
+    }
   };
 
-  return [image, takeScreenShot, error] as const;
+  const screenshotToClipboard = async (node: HTMLElement) => {
+    if (!node) {
+      throw new Error("You should provide correct html node.");
+    }
+    const blob = await toBlob(node);
+    navigator.clipboard.write([
+      new ClipboardItem({
+        [blob!.type]: blob!,
+      }),
+    ]);
+  };
+
+  return { image, takeScreenShot, screenshotToClipboard, error };
 };
 
 export { useScreenshot };
