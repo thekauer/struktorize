094e0424b4cff6fffafda34f98430c2dd1fa13f3
thekauer <thekauer@gmail.com>
2022-08-25 00:14:14 +0200

    feat: :sparkles: add empty body help text

---
+++ components/Ast/Function/EmptyBody/EmptyBody.atoms.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 10em;
+  padding: 1rem;
+  border: dashed 3px var(--s-selected);
+  color: var(--s-text);
+  line-height: 2;
+`;
+
+export const Kbd = styled.kbd`
+  padding: 0.2rem 0.4rem;
+  border-radius: 4px;
+  border: solid 1px var(--s-text);
+`;
+
+export const Mark = styled.mark`
+  background-color: var(--s-selected);
+  color: var(--s-text);
+  padding: 0.1rem 0.2rem;
+  border-radius: 4px;
+`;
---
+++ components/Ast/Function/EmptyBody/EmptyBody.tsx
@@ -0,0 +1,8 @@
+import * as S from "./EmptyBody.atoms";
+
+export const EmptyBody = () => (
+  <S.Container>
+    Press <S.Kbd>Enter</S.Kbd> or type <S.Mark>if</S.Mark> or{" "}
+    <S.Mark>loop</S.Mark> to get started.
+  </S.Container>
+);
