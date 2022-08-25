2f354f1c44e1fb44de5b116bfdad32cf7ff83f06
thekauer <thekauer@gmail.com>
2022-08-25 16:07:53 +0200

    refactor: :fire: default function should not have body

--- constants/defaultFunction.ts
+++ constants/defaultFunction.ts
@@ -2,14 +2,11 @@ import { FunctionAst } from "../lib/ast";
 
 export const DEFAULT_FUNCTION: FunctionAst = {
   signature: {
-    text: "\\text{main}(\\text{a}\\in{}\\mathbb{N})",
+    text: "\\text{main}(\\text{args}\\in{}\\mathbb{S}^*)",
     type: "signature",
     path: "signature",
   },
-  body: [
-    { path: "body.0", type: "statement", text: "\\text{a}:=1" },
-    { path: "body.1", type: "statement", text: "\\bold{return}\\;\\text{a}" },
-  ],
+  body: [],
   type: "function",
   path: "",
 };
