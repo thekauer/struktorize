0a97b86879cb829c310a69cfcc48053781bb76aa
thekauer <thekauer@gmail.com>
2022-08-28 22:16:49 +0200

    feat: :tada: add profile

--- components/SideMenu/Files/Files.tsx
+++ components/SideMenu/Files/Files.tsx
@@ -1,7 +1,9 @@
 import * as SM from "../SideMenu.atoms";
 import * as S from "./Files.atoms";
+import { useSession, signIn } from "next-auth/react";
 
 export const Files = () => {
+  const { status } = useSession();
   return (
     <SM.Container>
       <SM.Title>Files</SM.Title>
@@ -11,6 +13,14 @@ export const Files = () => {
         <S.MenuItem src={"/refresh.png"} />
         <S.MenuItem src={"/collapse_all.png"} />
       </S.Menu>
+      {status === "unauthenticated" && (
+        <>
+          <SM.Span>Sign in to save your structograms:</SM.Span>
+          <SM.Button onClick={() => signIn("google")}>Google</SM.Button>
+          <SM.Button onClick={() => signIn("github")}>Github</SM.Button>
+        </>
+      )}
+      {status === "authenticated" && <span>Your files will be here</span>}
     </SM.Container>
   );
 };
