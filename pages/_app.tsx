6c62e519b75d3300df5d21110832ddedc52ae400
thekauer <thekauer@gmail.com>
2022-08-28 21:02:08 +0200

    feat: :tada: add next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,19 @@ import "katex/dist/katex.min.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/Layout/Layout";
 import { ThemeProvider } from "../hooks/useTheme";
+import { SessionProvider } from "next-auth/react";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <ThemeProvider>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
