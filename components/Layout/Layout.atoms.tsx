312a75661d32d74009868aa640fa34c8d833b803
thekauer <thekauer@gmail.com>
2022-09-02 18:52:08 +0200

    fix: :lipstick: Remove padding from main

--- components/Layout/Layout.atoms.tsx
+++ components/Layout/Layout.atoms.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  height: 100vh;
+  height: 100%;
+  margin: 0;
 `;
 
 export const MainContainer = styled.div`
@@ -13,7 +14,6 @@ export const MainContainer = styled.div`
 
 export const Main = styled.main`
   flex: 1;
-  padding: 2.5rem;
   background: var(--dark);
   color: var(--text);
 `;
