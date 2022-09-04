d21901117088038d0e062bf50405dba0acc48763
thekauer <thekauer@gmail.com>
2022-09-04 15:24:37 +0200

    feat: :lock: use upstash adapter for next-auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,16 @@
+import { Redis } from "@upstash/redis";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
+
 export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(redis),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
