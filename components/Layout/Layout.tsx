99b9ad5964322f69f4b2e1b723086eb57237d066
Kauer András <andras.kauer@ibm.com>
2023-02-16 20:57:17 +0100

    feat: :bento: install `react-hot-toast`

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import FocusTrap from "focus-trap-react";
-import { ReactNode, useEffect, useState } from "react";
-import { useActiveItems } from "../../hooks/useActiveItems";
+import { ReactNode } from "react";
 import { AstProvider } from "../../hooks/useAST";
 import { useTheme } from "../../hooks/useTheme";
 import { CheatSheet } from "../CheatSheet/CheatSheet";
@@ -8,6 +7,7 @@ import { CommandPalette } from "../CommandPalette/CommandPalette";
 import { ExplorerProvider } from "../SideMenu/Files/Explorer/useExplorer";
 import { SideMenu } from "../SideMenu/SideMenu";
 import * as S from "./Layout.atoms";
+import { Toaster } from 'react-hot-toast';
 
 interface LayoutProps {
   children?: ReactNode;
@@ -15,10 +15,17 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   const { theme, showScope } = useTheme();
+  const toastOptions = {
+    style: {
+      background: 'var(--mid)',
+      color: 'var(--text)',
+    },
+  }
 
   return (
     <AstProvider showScope={showScope}>
       <ExplorerProvider>
+        <Toaster position="bottom-center" toastOptions={toastOptions} />
         <FocusTrap>
           <S.Container className={theme}>
             <SideMenu />
