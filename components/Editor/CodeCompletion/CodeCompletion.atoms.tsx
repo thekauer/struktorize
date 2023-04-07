1727061707af7468e2c40f79bb5c1bc5191cbfda
thekauer <thekauer@gmail.com>
2023-04-07 21:58:30 +0200

    feat: :tada: match cc's theme with structogram's

--- components/Editor/CodeCompletion/CodeCompletion.atoms.tsx
+++ components/Editor/CodeCompletion/CodeCompletion.atoms.tsx
@@ -8,10 +8,23 @@ export const Container = styled.div`
 export const RowContainer = styled.div`
   position: absolute;
   border: 1px solid black;
-  background: var(--mid);
+  background: var(--s-bg);
   max-height: 10rem;
   z-index: 10;
   overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 1em;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: var(--s-bg);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--s-hovered);
+    outline: 1px solid var(--s-hovered);
+  }
 `;
 
 export const Row = styled.div<{ selected?: boolean }>`
@@ -21,11 +34,12 @@ export const Row = styled.div<{ selected?: boolean }>`
   gap: 2rem;
   padding: 0.125rem;
   padding-left: 0.25rem;
-  ${({ selected }) => selected && `background: var(--blue-light)`}
+  color: var(--s-text);
+  ${({ selected }) => selected && `background: var(--blue-light)`};
 `;
 
 export const Tab = styled.span<{ selected?: boolean }>`
   font-weight: bold;
   ${({ selected }) =>
-    !selected ? "color: var(--text-secondary);" : "color: var(--text);"}
+    !selected ? "color: var(--text-secondary);" : "color: var(--s-text);"}
 `;
