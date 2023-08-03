025f81923495f6af7235fa5e5df6abc4898bd072
András Kauer <andraskauer@ibm.com>
2023-08-03 15:05:25 +0200

    feat: :tada: save temp file

---
+++ components/SideMenu/Files/Explorer/useTempFiles.tsx
@@ -0,0 +1,59 @@
+import { useAstState } from '@/hooks/useAST';
+import { getFunctionName } from '@/lib/abstractText';
+import { Ast, FunctionAst } from '@/lib/ast';
+import { File } from '@/lib/repository';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
+import { useCreateFile } from './useCreateFile';
+import { useFiles } from './useFiles';
+
+export const useTempFiles = () => {
+  const { status } = useSession();
+  const { ast, changed } = useAstState();
+  const { files } = useFiles();
+  const createFile = useCreateFile();
+
+  const TEMP_FILE_KEY = 'tempFile';
+
+  useEffect(() => {
+    if (status !== 'authenticated') return;
+
+    const getFile = () => {
+      const tempAst = localStorage.getItem(TEMP_FILE_KEY);
+      if (tempAst)
+        try {
+          const ast = JSON.parse(tempAst);
+          const path = `/${getFunctionName(
+            (ast as FunctionAst).signature.text,
+          )}`;
+          const type = 'file';
+
+          return { type, path, ast } as File;
+        } catch (e) {}
+    };
+
+    const maybeRenameFile = (file: File) => {
+      if (!files.some((f) => f.path === file.path)) return file;
+      let i = 1;
+      while (files.some((f) => f.path === `${file.path}${i}`) && i < 100) i++;
+
+      return {
+        ...file,
+        path: `${file.path}${i}`,
+      };
+    };
+
+    const ogFile = getFile();
+    if (!ogFile || files.length === 0) return;
+
+    const file = maybeRenameFile(ogFile);
+    createFile.mutate(file);
+    localStorage.removeItem(TEMP_FILE_KEY);
+  }, [status, ast, files]);
+
+  useEffect(() => {
+    if (status === 'unauthenticated' && changed) {
+      localStorage.setItem(TEMP_FILE_KEY, JSON.stringify(ast));
+    }
+  }, [status, ast, changed]);
+};
