4db192017125b9a9a64f38277698b37c964e984a
András Kauer <andraskauer@ibm.com>
2023-08-14 15:21:52 +0200

    feat: :tada: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import 'style/theme.css';
 import 'katex/dist/katex.min.css';
 
+import { Analytics } from '@vercel/analytics/react';
 import { Providers } from '@/lib/providers';
 import StyledComponentsRegistry from '@/lib/regisitry';
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Providers lang={lng}>
           <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
